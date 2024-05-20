5ce2792938d3c3eb865712c2e56eadde0d6324ec
peter-777 <32381921+peter-777@users.noreply.github.com>
2024-05-21 00:23:18 +0900

    Add  tasks to install docker-compose as a plugin.

--- roles/user_setup/files/append-bashrc.sh
+++ roles/user_setup/files/append-bashrc.sh
@@ -3,9 +3,6 @@ PS1="[\[\033[01;32m\]\u@\h \[\033[01;34m\]\W\[\033[00m\]]$ "
 # A socket for sharing the SSH-Agent between Windows and WSL through npiperelay.
 export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-relay/ssh-agent.sock
 
-# Podman Socket
-export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/podman/podman.sock
-
 # pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
