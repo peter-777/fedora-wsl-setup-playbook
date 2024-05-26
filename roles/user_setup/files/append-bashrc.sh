263d07c25d94f4b60daeac0d5313e5e3b9bed65e
peter-777 <32381921+peter-777@users.noreply.github.com>
2024-05-27 08:35:57 +0900

    Add support for enabling the Podman socket.

--- roles/user_setup/files/append-bashrc.sh
+++ roles/user_setup/files/append-bashrc.sh
@@ -3,6 +3,9 @@ PS1="[\[\033[01;32m\]\u@\h \[\033[01;34m\]\W\[\033[00m\]]$ "
 # A socket for sharing the SSH-Agent between Windows and WSL through npiperelay.
 export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-relay/ssh-agent.sock
 
+# Podman socket. Required for launching Testcontainers from Quarkus.
+export DOCKER_HOST=unix://$(podman info --format '{{.Host.RemoteSocket.Path}}')
+
 # pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
