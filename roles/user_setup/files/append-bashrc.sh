6230db54dfd4a1b69a347ce30d7c46f8a1126577
peter-777 <32381921+peter-777@users.noreply.github.com>
2024-08-06 16:01:37 +0900

    Commented out the `SSH_AUTH_SOCK` variable.

--- roles/user_setup/files/append-bashrc.sh
+++ roles/user_setup/files/append-bashrc.sh
@@ -1,7 +1,7 @@
 PS1="[\[\033[01;32m\]\u@\h \[\033[01;34m\]\W\[\033[00m\]]$ "
 
 # A socket for sharing the SSH-Agent between Windows and WSL through npiperelay.
-export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-relay/ssh-agent.sock
+# export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-relay/ssh-agent.sock
 
 # Podman socket. Required for launching Testcontainers from Quarkus.
 export DOCKER_HOST=unix://$(podman info --format '{{.Host.RemoteSocket.Path}}')
